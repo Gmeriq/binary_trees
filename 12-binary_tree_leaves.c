996d899c6b492cfee972af5c715902a5f5fa8930
Nate <49339539+Gmeriq@users.noreply.github.com>
2023-04-04 13:33:24 +0300

    styling

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,24 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - counts the leaves in a binary tree
+ * binary_tree_nodes - counts nodes with at least one child in a binary tree
  *
- * @tree: pointer to the root node of the tree to count the number of leaves
+ * @tree: pointer to the root node of the tree
  * Return: number of leaves
  */
 
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t count = 0;
 
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
+	if (tree->left || tree->right)
 		count++;
 
-	count += binary_tree_leaves(tree->left);
-	count += binary_tree_leaves(tree->right);
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
 
 	return (count);
 }
