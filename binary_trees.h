b119d578ca2549eb69cfa85c3593749ade406ac3
Nate <49339539+Gmeriq@users.noreply.github.com>
2023-04-04 15:20:31 +0300

    update styling

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,7 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
+/*Libraries*/
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,13 +15,13 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
@@ -49,4 +50,4 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /*BINARY_TREES_H*/
